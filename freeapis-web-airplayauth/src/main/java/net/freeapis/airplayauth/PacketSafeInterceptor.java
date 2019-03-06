135e8d71ab4f4a60c88cbdcd4c22e47977ef248b
wuqiang <wuqiang@lexing.ren>
2019-03-06 11:23:53 +0800

    添加设备认证记录接口

--- freeapis-web-airplayauth/src/main/java/net/freeapis/airplayauth/PacketSafeInterceptor.java
+++ freeapis-web-airplayauth/src/main/java/net/freeapis/airplayauth/PacketSafeInterceptor.java
@@ -3,7 +3,11 @@ package net.freeapis.airplayauth;
 import com.alibaba.fastjson.JSON;
 import net.freeapis.airplayauth.face.AuthHistoryService;
 import net.freeapis.airplayauth.face.constants.AirplayauthConstants;
+import net.freeapis.airplayauth.face.entity.AuthHistory;
+import net.freeapis.airplayauth.face.model.AuthInfoModel;
 import net.freeapis.core.foundation.constants.CoreConstants;
+import net.freeapis.core.foundation.sequence.SequenceGenerator;
+import net.freeapis.core.foundation.utils.ValidationUtil;
 import net.freeapis.core.rest.utils.ResponseHelper;
 import net.freeapis.core.rest.utils.ResponseModel;
 import net.freeapis.systemctl.face.DictionaryService;
@@ -13,6 +17,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.task.TaskExecutor;
@@ -21,6 +27,7 @@ import org.springframework.stereotype.Component;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
 
 /**
  * freeapis, Inc.
@@ -36,6 +43,8 @@ import javax.crypto.spec.SecretKeySpec;
 @Component
 public class PacketSafeInterceptor {
 
+    private static final Logger logger = LoggerFactory.getLogger(PacketSafeInterceptor.class);
+
     @Autowired
     private DictionaryService dictionaryService;
 
@@ -44,6 +53,9 @@ public class PacketSafeInterceptor {
 
     @Autowired
     private AuthHistoryService authHistoryService;
+
+    @Autowired
+    private SequenceGenerator sequenceGenerator;
 
     @Pointcut("execution(* net.freeapis.resource.airplayauth.DeviceAuthResourcesV1.*(..))")
     public void packetSafeGuarantee() {
@@ -62,6 +74,7 @@ public class PacketSafeInterceptor {
             authResponse = ResponseHelper.buildResponseModel(retVal);
         }catch (Exception e){
             e.printStackTrace();
+            logger.error(e.getMessage());
             authResponse = ResponseHelper.internal_server_error(e.getMessage());
         }
 
@@ -110,7 +123,30 @@ public class PacketSafeInterceptor {
 
     private void recordAuthHistory(final ResponseModel authResponse){
         taskExecutor.execute(() -> {
+            AuthHistory authHistory = new AuthHistory();
+            authHistory.setSequenceNBR(sequenceGenerator.getNextValue());
+            authHistory.setRecDate(new Date());
+            authHistory.setRecStatus(CoreConstants.COMMON_ACTIVE);
+            authHistory.setRecUserId(CoreConstants.SYSTEM.toString());
+            authHistory.setAuthTime(new Date());
 
+            AuthInfoModel authInfo = (AuthInfoModel) authResponse.getResult();
+            if(ValidationUtil.isEmpty(authInfo)){
+                authHistory.setAuthSuccess(CoreConstants.COMMON_N);
+                authHistory.setFailedMessage(authResponse.getMessage());
+            }else{
+                authHistory.setAuthSuccess(CoreConstants.COMMON_Y);
+                authHistory.setCompanyCode(authInfo.getCompanyCode());
+                authHistory.setCompanyName(authInfo.getCompanyName());
+                authHistory.setDeviceMac(authInfo.getDeviceMac());
+                authHistory.setMachineModel(authInfo.getMachineModel());
+            }
+            try {
+                authHistoryService.create(authHistory);
+            }catch (Exception e){
+                e.printStackTrace();
+                logger.error(e.getMessage());
+            }
         });
     }
 }
